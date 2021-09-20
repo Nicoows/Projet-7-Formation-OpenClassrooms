4a76b901e7d62db10434653c7efa1e32ac004a76
Nicolas Marsalet <nicolas.marsalet@gmail.com>
2021-09-20 14:55:59 +0200

    détails

--- FrontEnd/projet-7/src/main.js
+++ FrontEnd/projet-7/src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
 import '@fortawesome/fontawesome-free/css/all.css' 
 import '@fortawesome/fontawesome-free/js/all.js'
 
@@ -13,4 +12,4 @@ if (token) {
 }
 //createApp.prototype.$axios = axios;
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(router).mount('#app')
